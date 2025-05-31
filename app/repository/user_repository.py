f38478603e01a5fb165f9cbf814075a338e96ace
Komilcoder <yaxshilikovkomil@gmail.com>
2025-05-31 18:04:34 +0500

    repository fixed

--- app/repository/user_repository.py
+++ app/repository/user_repository.py
@@ -3,15 +3,14 @@ from sqlalchemy.future import select
 from typing import List, Optional
 from app.models.user import User
 from app.schemas.user import UserCreate, UserUpdate
-from app.repository.base import AbstractRepository
-
+from app.interface.abstract_interface import AbstractRepository
 
 
 class UserRepository(AbstractRepository[User]):
     def __init__(self, db: AsyncSession):
         self.db = db
 
-    async def get_user(self, user_id: int) -> Optional[User]:
+    async def get(self, user_id: int) -> Optional[User]:
         result = await self.db.execute(select(User).where(User.id == user_id))
         return result.scalars().first()
 
@@ -19,18 +18,18 @@ class UserRepository(AbstractRepository[User]):
         result = await self.db.execute(select(User).where(User.email == email))
         return result.scalars().first()
 
-    async def get_users(self, skip: int = 0, limit: int = 100) -> List[User]:
+    async def get_all(self, skip: int = 0, limit: int = 100) -> List[User]:
         result = await self.db.execute(select(User).offset(skip).limit(limit))
         return result.scalars().all()
 
-    async def create_user(self, user: UserCreate) -> User:
+    async def create(self, user: UserCreate) -> User:
         db_user = User(**user.dict())
         self.db.add(db_user)
         await self.db.commit()
         await self.db.refresh(db_user)
         return db_user
 
-    async def update_user(self, user_id: int, user_update: UserUpdate) -> Optional[User]:
+    async def update(self, user_id: int, user_update: UserUpdate) -> Optional[User]:
         db_user = await self.get_user(user_id)
         if not db_user:
             return None
@@ -40,7 +39,7 @@ class UserRepository(AbstractRepository[User]):
         await self.db.refresh(db_user)
         return db_user
 
-    async def delete_user(self, user_id: int) -> bool:
+    async def delete(self, user_id: int) -> bool:
         db_user = await self.get_user(user_id)
         if not db_user:
             return False
